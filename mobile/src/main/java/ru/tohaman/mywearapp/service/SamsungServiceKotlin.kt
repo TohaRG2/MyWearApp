9387de5d964261e6b1a845e4212bd6639648eba3
Tohaman <avrozovu@yandex.ru>
2020-12-13 19:19:10 +0300

    Add support Tizen watch

--- mobile/src/main/java/ru/tohaman/mywearapp/service/SamsungServiceKotlin.kt
+++ mobile/src/main/java/ru/tohaman/mywearapp/service/SamsungServiceKotlin.kt
@@ -1,13 +1,21 @@
 package ru.tohaman.mywearapp.service
 
 import android.content.Context
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.widget.Toast
 import com.acrcloud.rec.sdk.ACRCloudClient
 import com.acrcloud.rec.sdk.ACRCloudConfig
 import com.acrcloud.rec.sdk.IACRCloudListener
 import com.samsung.android.sdk.SsdkUnsupportedException
 import com.samsung.android.sdk.accessory.*
+import org.json.JSONException
+import org.json.JSONObject
 import ru.tohaman.mywearapp.DeveloperKey
+import ru.tohaman.mywearapp.data.MusicItem
+import ru.tohaman.mywearapp.data.musicDatabase
+import ru.tohaman.mywearapp.ioThread
 import timber.log.Timber
 import java.io.IOException
 import java.text.SimpleDateFormat
@@ -18,6 +26,7 @@ class SamsungServiceKotlin(private val mContext: Context) :
     IACRCloudListener {
 
     private var startTime: Long = 0
+    private var stopTime: Long = 0
 
     private var mMessage: SAMessage? = null
     private var mToast: Toast? = null
@@ -98,7 +107,7 @@ class SamsungServiceKotlin(private val mContext: Context) :
     }
 
     companion object {
-        private const val TAG = "HelloMessage(P)"
+        private const val TAG = "MyWearAppSAP"
     }
 
     init {
@@ -120,7 +129,7 @@ class SamsungServiceKotlin(private val mContext: Context) :
         }
         mMessage = object : SAMessage(this) {
             override fun onSent(peerAgent: SAPeerAgent, id: Int) {
-                Timber.d("TAG$ onSent(), id: $id ToAgent: ${peerAgent.peerId}")
+                Timber.d("$TAG onSent(), id: $id ToAgent: ${peerAgent.peerId}")
             }
 
             override fun onError(peerAgent: SAPeerAgent, id: Int, errorCode: Int) {
@@ -130,25 +139,22 @@ class SamsungServiceKotlin(private val mContext: Context) :
             override fun onReceive(peerAgent: SAPeerAgent, message: ByteArray) {
                 Timber.d("$TAG onReceive(), FromAgent : ${peerAgent.peerId} Message : ${String(message)}")
                 watchAgent = peerAgent
-                displayToast("MESSAGE RECEIVED", Toast.LENGTH_SHORT)
-                val calendar: Calendar = GregorianCalendar()
-                val dateFormat = SimpleDateFormat("yyyy.MM.dd aa hh:mm:ss.SSS", Locale.getDefault())
-                val timeStr = " " + dateFormat.format(calendar.time)
+//                displayToast("MESSAGE RECEIVED", Toast.LENGTH_SHORT)  //для сервиса не имеет смысла
                 val strToUpdateUI = String(message)
-                val str = "$strToUpdateUI $timeStr by MyWearKotlinService"
-
-                sendMessage2Wear(str)
+                //Если получили startRecognize - распознаем песню, иначе отправляем то, что получили, прибавив к этому текущее время
+                if (strToUpdateUI == "startRecognize") {
+                    startRecognize()
+                    sendMessage2Wear("?")
+                } else {
+                    val calendar: Calendar = GregorianCalendar()
+                    val dateFormat = SimpleDateFormat("yyyy.MM.dd aa hh:mm:ss.SSS", Locale.getDefault())
+                    val timeStr = " " + dateFormat.format(calendar.time)
+                    val str = "$strToUpdateUI $timeStr by MyWearKotlinService"
+                    sendMessage2Wear(str)
+                }
             }
         }
         arcCloudInit(mContext)
-    }
-
-    override fun onResult(p0: String?) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onVolumeChanged(p0: Double) {
-        TODO("Not yet implemented")
     }
 
     private fun arcCloudInit(ctx: Context) {
@@ -184,6 +190,75 @@ class SamsungServiceKotlin(private val mContext: Context) :
             }
             startTime = System.currentTimeMillis()
         }
+    }
+
+    override fun onResult(result: String?) {
+        Timber.d("onResult SamsungService")
+        if (mClient != null) {
+            mClient!!.cancel()
+            mProcessing = Processing.Waiting
+        }
+        stopTime = (System.currentTimeMillis() - startTime) / 1000
+
+        result?.let{
+            var tres = "\n"
+
+            var outTitle = ""
+            var outArtist = ""
+
+            try {
+                val j = JSONObject(result)
+                val j1 = j.getJSONObject("status")
+                val j2 = j1.getInt("code")
+                if (j2 == 0) {
+                    val metadata = j.getJSONObject("metadata")
+                    //
+                    if (metadata.has("music")) {
+                        val musics = metadata.getJSONArray("music")
+                        for (i in 0 until 1) {      //берем только одну запись из всех найденных (если найдены конечно)
+                            val tt = musics.get(i) as JSONObject
+                            outTitle = tt.getString("title")
+                            val artistt = tt.getJSONArray("artists")
+                            val art = artistt.get(0) as JSONObject
+                            outArtist = art.getString("name")
+                            tres = tres + (i + 1) + ".  Title: " + outTitle + "    Artist: " + outArtist + "\n"
+                        }
+                    }
+                    tres = tres + "\n\n" + result
+                } else {
+                    tres = result ?: "\n"
+                }
+            } catch (e: JSONException) {
+                tres = result ?: ""
+                e.printStackTrace()
+            }
+            sendMessage2Wear("$stopTime : $outArtist")
+            ioThread {
+                try {
+                    val dao = musicDatabase.musicDao
+                    dao.insert(MusicItem(0, outArtist, outTitle, stopTime, Date(), result))
+                } catch (e: IOException) {
+                    Timber.d("room.dao.exception")
+                }
+            }
+            if ((outArtist != "") or (outArtist != "?")) oneShotVibration()
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    private fun oneShotVibration() {
+        Timber.d("oneShortVibration SamsungService")
+        val vibrator = mContext.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            vibrator.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else {
+            vibrator.vibrate(500)
+        }
+
+    }
+
+    override fun onVolumeChanged(p0: Double) {
+//        stopTime = (System.currentTimeMillis() - startTime) / 1000
     }
 
     private fun sendMessage2Wear(str: String) {
